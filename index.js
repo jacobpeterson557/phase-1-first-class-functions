8c748b02158a14d6b190feb8e37e45db750f2135
+ <+>
2021-03-22 09:19:26 -0600

    commit tree 14918106806f3dbec699cd573ff6da0d84b163b1

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function receivesAFunction(callback){
+    callback()
+}
+
+function returnsANamedFunction(){
+    return receivesAFunction
+}
+
+function returnsAnAnonymousFunction(){
+    return () => {
+        console.log('Hello')
+    }
+}
+
